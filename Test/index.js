612f1b5b1913a0964bd9993fcc699d71199685b3
AntonioDragon <antonio300dragon@gmail.com>
2021-05-03 10:42:07 +0300

    Add TestProject for DevIT

---
+++ Test/index.js
@@ -0,0 +1,22 @@
+
+$(window).scroll(function(){
+    var $sections = $('section');
+$sections.each(function(i,el){
+   var top  = $(el).offset().top-100;
+   var bottom = top +$(el).height();
+   var scroll = $(window).scrollTop();
+   var id = $(el).attr('id');
+   if( scroll > top && scroll < bottom){
+       $('a.active').removeClass('active');
+       $('a[href="#'+id+'"]').addClass('active');
+
+   }
+})
+});
+
+$("nav").on("click","a", function (event) {
+   event.preventDefault();
+   var id  = $(this).attr('href'),
+       top = $(id).offset().top;
+   $('body,html').animate({scrollTop: top}, 50);
+});
